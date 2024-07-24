a1df5b56d496f5b8a8a3a1e037a3fdf33e996cc1
bilalq99 <133537558+bilalq99@users.noreply.github.com>
2024-07-23 22:14:11 -0500

    test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,27 @@
 import pytest
+from django.urls import reverse
 
-
+@pytest.mark.django_db
 def test_api_parse_succeeds(client):
-    # TODO: Finish this test. Send a request to the API and confirm that the
-    # data comes back in the appropriate format.
+    # This test verifies that the API successfully parses a valid address
     address_string = '123 main st chicago il'
-    pytest.fail()
+    response = client.get(reverse('parse_address'), {'address': address_string})
+    
+    assert response.status_code == 200
+    data = response.json()
+    assert 'input_string' in data
+    assert data['input_string'] == address_string
+    assert 'address_components' in data
+    assert 'address_type' in data
+    assert len(data['address_components']) > 0  # Ensure some components are parsed
 
-
+@pytest.mark.django_db
 def test_api_parse_raises_error(client):
-    # TODO: Finish this test. The address_string below will raise a
-    # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
+    # This test verifies that the API returns an error for an invalid address
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    response = client.get(reverse('parse_address'), {'address': address_string})
+    
+    assert response.status_code == 400
+    data = response.json()
+    assert 'error' in data
+    assert data['error'] == 'Invalid address format'
